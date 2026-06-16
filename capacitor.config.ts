99fcfc5e2f543134dc78ed5a7dfacae757618052
Mohaniish2208 <mohaniish5@gmail.com>
2026-06-16 05:36:03 -0400

    added android studio files, installed 'Android studio' in the machine. Also installed the Java Development Kit (JDK) for capacitor to read from it.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lifted.lexora',
+  appName: 'Lexora',
+  webDir: 'dist'
+};
+
+export default config;
