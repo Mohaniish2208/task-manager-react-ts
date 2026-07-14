f374ce141fcd97286d1518c299f6cb3ff0109cd5
Mohaniish2208 <mohaniish5@gmail.com>
2026-07-14 06:00:15 -0400

    Modified signup page and signinpage styling.

--- src/Pages/SignUpPage.tsx
+++ src/Pages/SignUpPage.tsx
@@ -1,4 +1,7 @@
 export default function SignUp() {
+  function handlePassword(str: string) {
+    if (str.length < 8) return "The password should be at least 8 characters long."
+  }
   return (
     <main>
       <section className="logo-container">
@@ -14,39 +17,44 @@ export default function SignUp() {
       <section className="form-section">
         <form className="form">
           <div className="name-container">
-            <label className="name">Full name</label>
-            <input className="full-name-input" type="text" placeholder="Full name" />
+            <label className="name">First name: </label>
+            <input className="full-name-input" type="text" placeholder="First name" />
+          </div>
+
+          <div className="name-container">
+            <label className="name">Last name: </label>
+            <input className="full-name-input" type="text" placeholder="Last name" />
           </div>
 
           <div className="email-container">
-            <label className="email">Email</label>
+            <label className="email-reg">Email: </label>
             <input className="email-input" type="text" placeholder="Email" />
           </div>
 
           <div className="phone-container">
-            <label className="phone">Phone</label>
+            <label className="phone">Phone: </label>
             <input className="phone-input" type="text" placeholder="Phone" />
           </div>
 
           <div className="password-container">
-            <label className="password">Password</label>
-            <input className="password-input" type="text" placeholder="Password" />
+            <label className="password-reg">Password: </label>
+            <input className="password-input-reg" type="text" placeholder="Password" />
           </div>
 
           <div className="password-confirm-container">
-            <label> Confirm Password</label>
+            <label> Confirm Password: </label>
             <input className="confirmation-input" type="text" placeholder="Confirm Password" />
           </div>
 
           <button type="submit" className="sign-up-btn">
-            Sign Up
+            Register
           </button>
         </form>
         <section>
           <div className="option">
-            <span> </span>
-            <span>OR</span>
-            <span> </span>
+            <span className="before"></span>
+            <span className="or">or</span>
+            <span className="after"></span>
           </div>
           <button type="button" className="google-signin-btn">
             Sign up with Google
